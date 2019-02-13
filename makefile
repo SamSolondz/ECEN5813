353aa7ed30d816398b0f7fb333ff025bce7e3330
SamSolondz <samuel.solondz@colorado.edu>
2019-02-13 10:18:32 -0700

    Working on make  Please enter the commit message for your changes. Lines starting

--- makefile
+++ makefile
@@ -1,3 +1,15 @@
 #makefile for project 1
-terminal: terminal.o
-	gcc -o terminal terminal.o
+#the compilier is gcc
+CC = gcc
+
+#compiler flags:
+#	-Wall	turns on most compiler warnings
+CFLAGS = -Wall
+
+TARGET = terminal.o display.o
+
+$(TARGET): $(TARGET).c
+	$(CC) $(CFLAGS) -o $(TARGET)
+
+clean:
+	$(RM) $(TARGET)
--- terminal.c
+++ terminal.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
-void main()
+int main(void)
 {
-	printf("Hello World!\n");
+
+	printf("-----------------------------------------------------\n");
+	printf("Welcome. Please select an option below:\n");
+	printf("\t1. Option 1\n");
+	printf("\t2. Option 2\n");
+	printf("\t3. Exit\n");
+	printf("---Number select: ");
+	
+	int input;
+	scanf("%d", &input);
+	if(input == 3)
+	{
+		printf("\nGoodbye!");
+		return 0;
+	}
 }
