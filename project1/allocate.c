39934a00ea64a295e7b90c923be35d42132f9fdb
SamSolondz <samuel.solondz@colorado.edu>
2019-02-13 23:32:20 -0700

    allocate() added.

---
+++ project1/allocate.c
@@ -0,0 +1,18 @@
+#include "allocate.h"
+
+char * allocate()
+{
+	printf("Enter the number of bytes you would like to allocate: ");
+	char byte_read[3];
+	scanf("%s", byte_read); 	//TODO: protect against a user input that is too long.
+	int byte_count = atol(byte_read);
+
+	char *ptr = (char *) malloc(byte_count * sizeof(char));
+	if(ptr == NULL)
+	{
+		printf("Memory could not be allocated.\n");
+		return NULL;
+	}
+	//printf("%d", ptr);
+	return ptr;
+}
--- project1/terminal.c
+++ project1/terminal.c
@@ -2,14 +2,18 @@
 #include <string.h>
 #include <stdbool.h>
 #include "help.h"
+#include "allocate.h"
 int main(void)
 {
 	bool exit = false;
 	printf("------------------------------------------------------------------\n");
 	printf("Enter a command. Type 'Help' to get a list of commands\n");
+
+	void * allocated;
+
 	while(exit == false)
 	{
-	        char command[16];
+	  char command[16];
 		printf("Input: ");
 	        scanf("%15s", command); 	//TODO: protect against a user input that is too long.
 		if(sizeof(command) > 20)
@@ -20,9 +24,14 @@ int main(void)
 		{
 			help();
 		}
+		else if(!strcmp(command, "allocate") | !strcmp(command, "Allocate"))
+		{
+			allocated = allocate();
+			printf("%p\n", allocated);
+		}
 		else if(!strcmp(command, "exit") | !strcmp(command, "Exit"))
 		{
-			printf("Goodbye\n");
+			printf("Bye!\n");
 			exit = true;
 			return 0;
 		}
