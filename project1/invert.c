fe4a4ac5a191b8694f422de9a43e73bb418f88b2
Sarah Stephany <sast7580@colorado.edu>
2019-02-16 14:33:12 -0700

    Sarah added free and invert

---
+++ project1/invert.c
@@ -0,0 +1,33 @@
+#include "invert.h"
+
+int invert()
+    // use XOR to invert all memory bits in specified area of memory
+    //(address and number of 32-bit words).  Also report time to perform op.
+{    
+    // get specified address from user
+    unsigned int *address;
+    unsigned int *destination;
+    int len = sizeof(address);
+    *address = 0xffffffffffffffffffffffff4444444444444444444444444aaaaaaaaaaaaaaaa5555555553;
+
+//	printf("Enter a 32 bit hex address without \"0x\" that you want to invert: \n");
+//	scanf("%x", &address);
+	printf("address:%p\n", &address);
+
+	printf("address0:%d\n", address[0]);
+	printf("address1:%d\n", address[1]);
+
+//	if(!isxdigit(&address))
+    {
+//	    printf("\nYou need to enter in hex.\n\n");
+//	    printf("Enter a 32 bit hex address without \"0x\" where you want the data saved: \n");
+//	    scanf("%x", &address);
+	}
+
+    for(int i = 0; i < len; i++)	
+    {
+        destination[i] ^= address[i];
+    }
+
+	printf("Destination = %d \n", destination);
+}
