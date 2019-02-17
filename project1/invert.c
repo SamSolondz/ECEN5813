5493476a6a49575d580a66878c473d87d511f815
Sarah Stephany <sast7580@colorado.edu>
2019-02-16 22:51:34 -0700

    Sarah updated makefile, invert and write

--- project1/invert.c
+++ project1/invert.c
@@ -1,33 +1,59 @@
 #include "invert.h"
 
-int invert()
+void invert()
     // use XOR to invert all memory bits in specified area of memory
     //(address and number of 32-bit words).  Also report time to perform op.
-{    
+{
     // get specified address from user
-    unsigned int *address;
-    unsigned int *destination;
-    int len = sizeof(address);
-    *address = 0xffffffffffffffffffffffff4444444444444444444444444aaaaaaaaaaaaaaaa5555555553;
+    unsigned int *address = 0;
+    unsigned int data[] = {1,2,3,7,0,9};
+    int s = sizeof(data) / sizeof(int);
+    unsigned int inverted[s];
+    int last = sizeof(data) / sizeof(data[0]);
+    int len = last;
 
-//	printf("Enter a 32 bit hex address without \"0x\" that you want to invert: \n");
-//	scanf("%x", &address);
-	printf("address:%p\n", &address);
+	printf("Enter a 32 bit hex address without \"0x\" that you want to invert: \n");
+	scanf("%p", &address);
 
-	printf("address0:%d\n", address[0]);
-	printf("address1:%d\n", address[1]);
-
-//	if( (((addr |= 0x20) >= '0') && (addr <= '9')) || ((addr >= 'a') && (addr <= 'f'))  )
+//	if( ((*addr <= '0') || (*addr >= '9')) || ((*addr <= 'a') || (*addr >= 'f')) )
     {
 //	    printf("\nYou need to enter in hex.\n\n");
-//	    printf("Enter a 32 bit hex address without \"0x\" where you want the data saved: \n");
-//	    scanf("%x", &address);
 	}
 
-    for(int i = 0; i < len; i++)	
-    {
-        destination[i] ^= address[i];
-    }
+    *(int*)&address = data[0];	
 
-	printf("Destination = %d \n", destination);
+//	printf("len: %d\n", len);
+//	printf("last: %d\n", last);
+	printf("address: %p\n", &address);
+	
+	// print original data
+	printf("data in address = 0x");
+	for (int i = 0; i < len; i++) 
+    { 
+        data[i];
+        printf("%X", data[i]);
+    } 
+    printf("\n");
+    
+    // invert and print inverted data
+    printf("data in inverted: 0x");
+    for (int i = 0; i < len; i++) 
+    { 
+        inverted[i] = data[last];
+        last--;
+        printf("%X", inverted[i]);
+    } 
+        printf("\n\n");
+}
+
+int main()
+{
+    // calculate function time (geeksforgeeks.org)
+	clock_t timer; 
+    timer = clock(); 
+    invert(); 
+    timer = clock() - timer; 
+    double time_taken = ((double)timer)/CLOCKS_PER_SEC; // in seconds 
+  
+    printf("program took %f seconds to execute \n", time_taken); 
 }
--- project1/write.c
+++ project1/write.c
@@ -1,43 +1,30 @@
 #include "write.h"
 
-int write(unsigned int *address, unsigned int value);
+int write()
+{   
+	// get specified value and specified address from user
+    unsigned int *addr = 0; 
+    unsigned int val = 0;
 
-int main()
-{
-    // get specified value and specified address from user
-    unsigned int *addr; 
-    unsigned int val;
-
-	printf("Enter a 32 bit hex address without \"0x\" where you want the data saved: \n");
-	scanf("%x", &addr);
+	printf("Enter a 32-bit hex address without \"0x\" where you want the data saved. (ex f4ac23df): \n");
+	scanf("%p", &addr);
 	
-//	if( (((addr |= 0x20) >= '0') && (addr <= '9')) || ((addr >= 'a') && (addr <= 'f'))  )
+//	if( ((*addr <= '0') || (*addr >= '9')) || ((*addr <= 'a') || (*addr >= 'f')) )
     {
 //	    printf("\nYou need to enter in hex.\n\n");
-//	    printf("Enter a 32 bit hex address without \"0x\" where you want the data saved: \n");
-//	    scanf("%x", &addr);
 	}
 	
-	printf("Enter a 32 bit hex data without \"0x\" you want to save: \n");
+	printf("Enter a 32-bit hex data without \"0x\" you want to save: \n");
 	scanf("%x", &val);
 	
-//	if( (((addr |= 0x20) >= '0') && (addr <= '9')) || ((addr >= 'a') && (addr <= 'f'))  )
-	{
+//	if( ((*addr <= '0') || (*addr >= '9')) || ((*addr <= 'a') || (*addr >= 'f')) )
+    {
 //	    printf("\nYou need to enter in hex.\n\n");
-//	    printf("Enter a 32 bit hex data without \"0x\" you want to save: \n");
-//	    scanf("%x", &val);
 	}
+
+    *(int*)&addr = val;	
+	
 	printf("Data = 0x%X \n", val);
-	printf("Address = 0x%X \n", addr);
-
-    int ret = write(addr, val);
-}
-
-// function to write a specified value to a specified address
-int write(unsigned int *address, unsigned int value)
-{
-    int *addrPtr = address;
-    *addrPtr = value;
-    
-    printf("*addr: %x\n", addrPtr);
+	printf("Address = %p\n", &addr);
+	printf("Data at address = 0x%X \n", addr);
 }
