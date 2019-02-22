3b612103eb4882e84aa7a822d60eb8d8b4abca4b
SamSolondz <samuel.solondz@colorado.edu>
2019-02-22 07:34:53 -0700

    Display working

--- project1/displaymem.c
+++ project1/displaymem.c
@@ -2,23 +2,23 @@
 
 void displaymem()
 {
-  void * addr = 0;
+  unsigned long * addr = 0;
   unsigned long read = 0;
-  unsigned long numWords = 0;
+  int numWords = 0;
 
   // get address
   printf("Enter the starting address of the data to display (ex 0xffffffff): \n");
-  scanf("%lu", &read);
-  addr = (void * ) read;
+  scanf("%li", &read);
+  addr = (unsigned long * ) read;
 
   // get number of words
   printf("Enter the number of 32-bit words to display: \n");
-  scanf("%lu", &numWords);
+  scanf("%d", &numWords);
 
   // print data
   for(int i = 0; i < numWords; i++)
   {
-      printf("Address: %p & Data: %#08lX\n", addr + i, *(addr + i);
+      printf("Address: %p & Data: %#08lX\n", addr + i, *(addr + i));
   }
   return;
 }
