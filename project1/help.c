14b2b94564b27dcb6bf12479d90db81bcc8ac659
Sarah Stephany <sast7580@colorado.edu>
2019-03-08 15:54:59 -0700

    Updated help

--- project1/help.c
+++ project1/help.c
@@ -4,12 +4,12 @@ void help()
 {
 	printf("(Help)\n");
 	printf("---Commands available---\n");
-	printf("'Allocate' memory\t Allocate a block of memory. User specifies the number of or words to be allocated.\n");
-	printf("'Free' memory\t\t Release the previously allocated block.\n");
-	printf("'Display' memory words\t Display a block of memory in hex format. User may specify an address and the number of words to display.\n");
-	printf("'Write' memory words\t User specifies an address to modify.\n");
-	printf("'Invert' blocks\t\t Invert all memory bits in a specified area of memory. User specifies block.\n");
-	printf("'Pattern'\t\t Write a pseudo-random pattern in specified area of memory using specified seed value. User specifies address and length of memory.\n");
-	printf("'Verify'\t Verify a pseudo-random patter in specified area of memory using specified seed value. User specifies address and length of memory. \n\n");
+	printf("'Allocate'\t\t Allocate a block of memory. User specifies the number of words to be allocated.\n");
+	printf("'Free'\t\t Release the previously allocated block of memory.\n");
+	printf("'Display'\t\t Display a block of memory in hex format. User specifies an address and number of words to display.\n");
+	printf("'Write'\t\t User specifies an address to modify and data to write to that address.\n");
+	printf("'Invert'\t\t Invert all memory bits at a specified memory address.\n");
+	printf("'Pattern'\t\t Write a pseudo-random pattern of a specified length at a specified memory address using a specified seed value.\n");
+	printf("'Verify'\t\t Verify a pseudo-random pattern of a specified length at a specified memory address using a specified seed value.\n\n");
 	return;
 }
