c96879866bf3186137901b8521f9023275d4dc86
Sarah Stephany <sast7580@colorado.edu>
2019-03-10 22:15:33 -0600

    Sarah added maxallo.c to test allocation limit

---
+++ project1/maxallo.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+// taken from deep c secrets to test maximum amount we can allocate
+
+main(){
+	int Mb = 0;
+	while (malloc(1<<20))
+	{
+		 ++Mb;
+		printf("Allocated %d Mb total\n", Mb);
+	}
+}
